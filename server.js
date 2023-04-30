766fcafc1392fa781d0248439ed0ed142bd64ad0
yuviSingla <131614988+yuviSingla@users.noreply.github.com>
2023-04-30 14:47:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+/*********************************************************************************
+* WEB322: Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+* 
+* Name: ______yuvi________________ Student ID: ____126567213__________ Date: ________________
+*
+* Online (cyclic) URL: https://odd-bat-beanie.cyclic.app and https://app.cyclic.sh/#/app/yuvisingla-helloworld/builds
 
- WEB322: Assignment 1
- I declare that this assignment is my own work in accordance with Seneca Academic Policy.
- No part of this assignment has been copied manually or electronically from any other source
- (including web sites) or distributed to other students. *
-Name: yuvi Student ID: 126567213 Date: May 18, 2023 *
-Online (Heroku) URL: https://odd-bat-beanie.cyclic.app
-
+********************************************************************************/
 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
