4ac0ea22c653dc17271a04f1f9dcaa5d94b18e64
yuviSingla <131614988+yuviSingla@users.noreply.github.com>
2023-04-30 01:46:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,16 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("student name - yuvi student id: 126567213"
-            /*********************************************************************************
+    res.send("student name - yuvi student id: 126567213"     
 * WEB322: Assignment 1
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
 * No part of this assignment has been copied manually or electronically from any other source
 * (including web sites) or distributed to other students.
 * 
-* Name: ______________________ Student ID: ______________ Date: ________________
+* Name: _________yuvi_____________ Student ID: _______126567213_______ Date: ________________
 *
-* Online (Heroku) URL: _______________________________________________________
-*
-********************************************************************************/);
+* Online (Heroku) URL: __________________________________https://app.cyclic.sh/#/app/yuvisingla-helloworld/builds_____________________
+);
 });
 
 // setup http server to listen on HTTP_PORT
