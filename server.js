c49557302c97549825c1fad9ad54a2c404094075
yuviSingla <131614988+yuviSingla@users.noreply.github.com>
2023-04-30 01:49:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ var app = express();
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
     res.send("student name - yuvi student id: 126567213");
+});
+
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT);
