eb0e198f2d925c9ab1d5b470af98224638a54f97
yuviSingla <131614988+yuviSingla@users.noreply.github.com>
2023-04-30 01:51:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+/*********************************************************************************
+* WEB322: Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students. *
+* Name: yuvi Student ID: 126567213 Date: May 18, 2023 *
+* Online (Heroku) URL: https://odd-bat-beanie.cyclic.app
+* ********************************************************************************/
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("student name - yuvi student id: 126567213");
+    res.send(" yuvi - 126567213");
 });
 
 // setup http server to listen on HTTP_PORT
